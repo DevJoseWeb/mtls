ea68d84f5df65a7d0a6f72c2e0dee7d81e0f7abd
bgops <bgops@test2.klin-pro.com>
2018-06-23 12:51:18 -0700

    added documentation

--- req/struct.go
+++ req/struct.go
@@ -3,12 +3,12 @@ package klinreq
 import ()
 
 type ReqInfo struct {
-	Cert   string
-	Key    string
-	Dest   string
-	Dport  string
-	Trust  string
-	Method string
-	Route  string
-	File   string
+	Cert   string // The cert for mtls
+	Key    string // The key for mtls
+	Dest   string // The destination address. It has to be hostname
+	Dport  string // The destination address port
+	Trust  string // The trusted CA cert
+	Method string // The req method, POST/PATCH etc...
+	Route  string // The route, by default its "/" it can be "/api"
+	File   string // If you are sending file specify the file you are sending.
 }
